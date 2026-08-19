26e97bfb11390b514f82c53be215e69e095dd096
Daria <daria.agafonova.cv@gmail.com>
2026-08-19 03:20:57 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# plugins
+Custom Plugins for SP
---
+++ custom_exporter.py
@@ -0,0 +1,5 @@
+# Painter plugin for exporting textures from Substance Painter to a custom format.
+import substance_painter
+
+# 3rd party UI lib import
+from PySide6.QtWidgets import QWidget
