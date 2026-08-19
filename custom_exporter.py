0ba93aee0818d1cb5d7c4864eb43213856e97531
Daria <daria.agafonova.cv@gmail.com>
2026-08-19 05:18:03 +0300

    Add CustomExporter UI and lifecycle
    Introduce a CustomExporter class that creates and manages a QWidget for the plugin UI. Adds module-level custom_exporter, start_plugin and close_plugin functions to register/deregister the widget with substance_painter.ui. Also includes a __main__ entrypoint and minor comment/import formatting cleanup.

--- custom_exporter.py
+++ custom_exporter.py
@@ -1,5 +1,30 @@
-# Painter plugin for exporting textures from Substance Painter to a custom format.
+# Painter API import
 import substance_painter
 
-# 3rd party UI lib import
+#3d party UI lib import
 from PySide6.QtWidgets import QWidget
+
+
+custom_exporter = None
+
+class CustomExporter:
+   def __init__(self):
+    self.widget = QWidget()
+    self.widget.setWindowTitle("Custom Exporter")
+    substance_painter.ui.add_dock_widget(self.widget)
+
+   def delete_widget(self):
+      if self.widget is not None:
+         substance_painter.ui.delete_ui_element(self.widget)
+
+def start_plugin():
+   global custom_exporter
+   custom_exporter = CustomExporter()
+
+def close_plugin():
+   global custom_exporter
+   custom_exporter.delete_widget()
+
+   if __name__ == "__main__":
+      start_plugin()
+   
