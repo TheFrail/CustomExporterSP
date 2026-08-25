cbad4e9bcb11b3f26f5281912e8354837371c48b
Daria <daria.agafonova.cv@gmail.com>
2026-08-26 02:15:06 +0300

    Use Substance Painter logging; remove test buttons
    Replace debug print() calls with substance_painter.logging.log so messages appear in Substance Painter's logging system. Remove the Info/Warning/Error test buttons and their click handlers from the UI. Minor cleanup: log asset-type and personal-export checkbox state instead of printing.

--- custom_exporter.py
+++ custom_exporter.py
@@ -44,26 +44,11 @@ class CustomExporter:
       self.export_btn = QPushButton("Export")
       self.main_layout.addWidget(self.export_btn)
 
-
-      #Test Button
-      self.info_btn = QPushButton("Info")
-      self.warning_btn = QPushButton("Warning")
-      self.error_btn = QPushButton("Error")
-
-      self.main_layout.addWidget(self.info_btn)
-      self.main_layout.addWidget(self.warning_btn)
-      self.main_layout.addWidget(self.error_btn)
-
-
-
    def connect_slots(self):
       self.export_btn.clicked.connect(self.on_export_requested)
       self.personal_export_cb.stateChanged.connect(self.on_toggle_personal_export)
       self.asset_type_cmbx.currentIndexChanged.connect(self.on_asset_type_chaged)
 
-      self.info_btn.clicked.connect(self.on_info_clicked)
-      self.warning_btn.clicked.connect(self.on_warning_clicked)
-      self.error_btn.clicked.connect(self.on_error_clicked)
 
    def show_ui_widget(self):
       plugin = substance_painter_plugins.plugins.get("Custom Exporter", None)
@@ -80,28 +65,22 @@ class CustomExporter:
          substance_painter.ui.delete_ui_element(self.widget)
 
    def on_export_requested(self):
-      print("Export button clicked")
+      substance_painter.logging.log(substance_painter.logging.INFO, "Custom Exporter", "Export button clicked")
 
    def on_toggle_personal_export(self, state):
       check_state = QtCore.Qt.CheckState(state)
 
       if check_state == QtCore.Qt.CheckState.Checked:
-        print("Personal Export checkbox is checked")
+        
+        substance_painter.logging.log(substance_painter.logging.INFO, "Custom Exporter", "Personal Export checkbox is checked")
+   
       else:
-        print("Personal Export checkbox is unchecked")
+        substance_painter.logging.log(substance_painter.logging.INFO, "Custom Exporter", "Personal Export checkbox is unchecked")
 
    def on_asset_type_chaged(self, current_index):
       current_asset_type_text = self.asset_type_cmbx.itemText(current_index)
-      print(f"asset Type has been changed. Now it's set to {current_asset_type_text}")
-
-   def on_info_clicked(self):
-      substance_painter.logging.log(substance_painter.logging.INFO, "Custom Exporter", "This is an Info log")
-
-   def on_warning_clicked(self):
-      substance_painter.logging.log(substance_painter.logging.WARNING, "Custom Exporter", "This is an Warning log")
-
-   def on_error_clicked(self):
-      substance_painter.logging.log(substance_painter.logging.ERROR, "Custom Exporter", "This is an Error log")
+      substance_painter.logging.log(substance_painter.logging.INFO, "Custom Exporter", f"asset Type has been changed. Now it's set to {current_asset_type_text}")
+      
 
 
 
